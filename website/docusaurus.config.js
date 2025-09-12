992b96464a52c61f5b2bdf9c795ed5e2ea51bef2
omrishiv <327609+omrishiv@users.noreply.github.com>
2025-09-12 10:20:29 -0700

    create redirects from resources to guidance
    Signed-off-by: omrishiv <327609+omrishiv@users.noreply.github.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -6,109 +6,119 @@ const darkCodeTheme = require('prism-react-renderer').themes.dracula;
 
 /** @type {{onBrokenLinks: string, organizationName: string, plugins: string[], title: string, url: string, onBrokenMarkdownLinks: string, i18n: {defaultLocale: string, locales: string[]}, trailingSlash: boolean, baseUrl: string, presets: [string,Options][], githubHost: string, tagline: string, themeConfig: ThemeConfig & UserThemeConfig & AlgoliaThemeConfig, projectName: string}} */
 const config = {
-  title: 'AI on EKS',
-  tagline: 'Supercharge your AI/ML Journey with Amazon EKS',
-  url: 'https://awslabs.github.io',
-  baseUrl: '/ai-on-eks/',
-  trailingSlash: false,
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/header-icon.png',
+    title: 'AI on EKS',
+    tagline: 'Supercharge your AI/ML Journey with Amazon EKS',
+    url: 'https://awslabs.github.io',
+    baseUrl: '/ai-on-eks/',
+    trailingSlash: false,
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/header-icon.png',
 
-  organizationName: 'awslabs',
-  projectName: 'ai-on-eks',
-  githubHost: 'github.com',
+    organizationName: 'awslabs',
+    projectName: 'ai-on-eks',
+    githubHost: 'github.com',
 
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/awslabs/ai-on-eks/blob/main/website/',
-        },
-        theme: {
-          customCss: [
-            require.resolve('./src/css/custom.css'),
-            require.resolve('./src/css/fonts.css'),
-          ],
-        },
-      }),
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    editUrl: 'https://github.com/awslabs/ai-on-eks/blob/main/website/',
+                },
+                theme: {
+                    customCss: [
+                        require.resolve('./src/css/custom.css'),
+                        require.resolve('./src/css/fonts.css'),
+                    ],
+                },
+            }),
+        ],
     ],
-  ],
 
-  themes: ['@docusaurus/theme-mermaid'],
+    themes: ['@docusaurus/theme-mermaid'],
 
-  markdown: {
-    mermaid: true,
-  },
+    markdown: {
+        mermaid: true,
+    },
 
-  themeConfig:
+    themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      announcementBar: {
-        id: 'genai-workshop-banner',
-        content:
-          'GenAI on EKS workshop series! <a target="_blank" rel="noopener noreferrer" href="https://aws-experience.com/emea/smb/events/series/get-hands-on-with-amazon-eks?trk=9be4af2e-2339-40ae-b5e9-57b6a7704c36&sc_channel=el" style="color: #ffffff; text-decoration: underline; font-weight: bold; margin-left: 10px;">Register now →</a>',
-        backgroundColor: '#667eea',
-        textColor: '#ffffff',
-        isCloseable: true,
-      },
-      mermaid: {
-        theme: { light: 'neutral', dark: 'forest' },
-        options: {
-          maxTextSize: 50000,
-        },
-      },
-      navbar: {
-        // title: 'AIoEKS',
-        logo: {
-          alt: 'AIoEKS Logo',
-          src: 'img/header-icon.png',
-        },
-        items: [
-          { type: 'doc', docId: 'infra/ai-ml/index', position: 'left', label: 'Infrastructure' },
-          { type: 'doc', docId: 'blueprints/index', position: 'left', label: 'Blueprints' },
-          { type: 'doc', docId: 'guidance/index', position: 'left', label: 'Guidance' },
-          { href: 'https://github.com/awslabs/ai-on-eks', label: 'GitHub', position: 'right' },
-        ],
-      },
-      colorMode: {
-        defaultMode: 'light',
-        disableSwitch: false,
-        respectPrefersColorScheme: true,
-      },
-      docs: {
-        sidebar: {
-          hideable: true,
-          autoCollapseCategories: true,
-        }
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Get Involved',
-            items: [{ label: 'Github', href: 'https://github.com/awslabs/ai-on-eks' }],
-          },
-        ],
-        copyright: `Built with ❤️ at AWS  <br/> © ${new Date().getFullYear()} Amazon.com, Inc. or its affiliates. All Rights Reserved`,
-      },
+        ({
+            announcementBar: {
+                id: 'genai-workshop-banner',
+                content:
+                    'GenAI on EKS workshop series! <a target="_blank" rel="noopener noreferrer" href="https://aws-experience.com/emea/smb/events/series/get-hands-on-with-amazon-eks?trk=9be4af2e-2339-40ae-b5e9-57b6a7704c36&sc_channel=el" style="color: #ffffff; text-decoration: underline; font-weight: bold; margin-left: 10px;">Register now →</a>',
+                backgroundColor: '#667eea',
+                textColor: '#ffffff',
+                isCloseable: true,
+            },
+            mermaid: {
+                theme: {light: 'neutral', dark: 'forest'},
+                options: {
+                    maxTextSize: 50000,
+                },
+            },
+            navbar: {
+                // title: 'AIoEKS',
+                logo: {
+                    alt: 'AIoEKS Logo',
+                    src: 'img/header-icon.png',
+                },
+                items: [
+                    {type: 'doc', docId: 'infra/ai-ml/index', position: 'left', label: 'Infrastructure'},
+                    {type: 'doc', docId: 'blueprints/index', position: 'left', label: 'Blueprints'},
+                    {type: 'doc', docId: 'guidance/index', position: 'left', label: 'Guidance'},
+                    {href: 'https://github.com/awslabs/ai-on-eks', label: 'GitHub', position: 'right'},
+                ],
+            },
+            colorMode: {
+                defaultMode: 'light',
+                disableSwitch: false,
+                respectPrefersColorScheme: true,
+            },
+            docs: {
+                sidebar: {
+                    hideable: true,
+                    autoCollapseCategories: true,
+                }
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'Get Involved',
+                        items: [{label: 'Github', href: 'https://github.com/awslabs/ai-on-eks'}],
+                    },
+                ],
+                copyright: `Built with ❤️ at AWS  <br/> © ${new Date().getFullYear()} Amazon.com, Inc. or its affiliates. All Rights Reserved`,
+            },
 
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-        additionalLanguages: ['bash', 'yaml', 'hcl', 'json', 'python', 'javascript', 'typescript', 'jsx', 'tsx'],
-      },
-    }),
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+                additionalLanguages: ['bash', 'yaml', 'hcl', 'json', 'python', 'javascript', 'typescript', 'jsx', 'tsx'],
+            },
+        }),
 
-    plugins: [require.resolve('docusaurus-lunr-search')],
+    plugins: [require.resolve('docusaurus-lunr-search'),
+        ['@docusaurus/plugin-client-redirects', {
+            createRedirects(existingPath) {
+                if (existingPath.includes('/docs/guidance')) {
+                    return [
+                        existingPath.replace('/docs/guidance', '/docs/resources'),
+                    ];
+                }
+                return undefined; // Return a falsy value: no redirect created
+            },
+        }]],
 };
 
 module.exports = config;
